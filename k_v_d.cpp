e5d37ff2f01c544192e2b8e750b1c6905c60b0d5
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-07-19 16:37:20 +0600

    keyword, variable & data type

---
+++ k_v_d.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    
+    int age=21;
+    float age_f=21.3;
+    char name='H';
+    cout<<"My name is "<<name<<"."<<endl<<"My age is"<<age<<endl;
+    
+
+    int num=10,num2=13,num3=14;
+    double num4=14.5,num5=15.5;
+    cout<<num<<endl<<num2<<endl<<num3<<endl<<num4<<endl<<num5;
+
+}
