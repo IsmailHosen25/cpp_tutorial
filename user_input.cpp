4c01bab7c29f2f2cc53dcc56329a91186d4b77a5
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-07-19 17:00:10 +0600

    user input

---
+++ user_input.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int age;
+    // cout<<"Enter your age: ";
+    // cin>>age;
+    // cout<<"Enter your Name: ";
+    // char name[6];
+    // cin>>name;
+    // cout<<"My Name is "<<name<<",and age- "<<age;
+
+
+    char name[21];
+    cout<<"Enter your Name: ";
+    cin>>name;
+    int math,phy,che;
+    cout<<"Enter Your math, phy and che Number: ";
+    cin>>math>>phy>>che;
+    cout<<"Your Name is "<<name<<endl<<"Your Math Number is "<<math<<endl;
+    cout<<"Your Phy number is "<<phy<<endl<<"Your Che Number is "<<che<<endl;
+}
